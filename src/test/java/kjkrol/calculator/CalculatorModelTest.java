e05a66a97153a1e9ddcf2bc4ce3b579ab513bbab
Karol Król <Karol.Krol@vimn.com>
2017-12-14 12:41:37 +0100

    add unit test for calculator model

---
+++ src/test/java/kjkrol/calculator/CalculatorModelTest.java
@@ -0,0 +1,27 @@
+package kjkrol.calculator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorModelTest {
+    @Test
+    public void calculate() throws Exception {
+        
+        // Given
+        StringBuilder output = new StringBuilder();
+        CalculatorModel calculatorModel = new CalculatorModel(output::append);
+        calculatorModel.insertSymbol("1");
+        calculatorModel.insertSymbol("0");
+        calculatorModel.selectOperation(CalculatorOperation.ADD);
+        calculatorModel.insertSymbol("7");
+        output.setLength(0);
+
+        // When
+        calculatorModel.calculate();
+
+        // Then
+        assertEquals("17.0", output.toString());
+    }
+
+}
